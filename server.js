e1e134a1461d90d65d2bcc0d58f6d79f4bafd3b0
Rajat Kharbandaa <kharbandarajat11@gmail.com>
2026-03-21 23:52:02 +0530

    delete extra image and changes in server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.listen(process.env.PORT,process.env.HOST,(err)=>{
     if(err){
         console.log("err in starting the server"+err);
     }else{
+        //load server 
         console.log(`server started successfully at http://${process.env.HOST}:${process.env.PORT}`);
     }
 })
