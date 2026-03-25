bad6a95c5eb6250cc45247530bc0eaa6dc6dbc20
Rajat Kharbandaa <kharbandarajat11@gmail.com>
2026-03-25 20:57:52 +0530

    improve the code of database connection

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose")
 const connectDB = async()=>{
     await mongoose.connect(process.env.MONGO_DB)
     console.log("database connected succesfully");
-}
+} // this function  connect the database
 
 module.exports=connectDB;
